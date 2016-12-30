04a0f8e9f84ecc3aef90163a4554ad4ce371fb8d
tjforryan <thomas.forryan@sky.uk>
2016-12-30 12:16:48 +0000

    Made it easy to specify what tests I want to run.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var argv = require('yargs').argv;
 
 gulp.task('unitTests', function () {
-  return gulp.src('./tests/**/*.js', {read: false})
+  const day = argv.day !== true ? `day_${argv.day}` : '**';
+  const part = argv.part !== true ? `day_${argv.day}_${argv.part}.test` : '*';
+
+  console.log('PATH: ', `./tests/${day}/${part}.js`)
+
+  return gulp.src(`./tests/${day}/${part}.js`, {read: false})
     // gulp-mocha needs filepaths so you can't have any plugins before it
     .pipe(mocha({reporter: 'nyan'}));
 });
